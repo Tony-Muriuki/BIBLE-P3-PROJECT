bdd19b2cae3b4827a40e0181eec551f60dda5bce
tonymk0987 <k.tony.muriuki@gmail.com>
2024-06-15 01:07:59 +0300

    Initial commit

---
+++ lib/Bible.py
@@ -0,0 +1,33 @@
+from __init__ import CURSOR, CONN
+
+class Bible:
+
+    def __init__(self, testament, category, id=None):
+        self.id = id
+        self.testament = testament
+        self.category = category
+
+    def __repr__(self):
+        return f"<Bible {self.id}: {self.testament}, {self.category}>"
+    
+    @classmethod
+    def create_table(cls):
+        """ CREATES A NEW TABLE TO PERSIST THE ATTRIBUTES OF BIBLE INSTANCES """
+        sql = """
+            CREATE TABLE IF NOT EXISTS bibles (
+            id INTEGER PRIMARY KEY,
+            testament TEXT,
+            category TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+        
+        
+    @classmethod
+    def drop_table(cls):
+        """ DROPS THE TABLE THAT PERSISTS BIBLE instances """
+        sql = """
+            DROP TABLE IF EXISTS bibles;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
