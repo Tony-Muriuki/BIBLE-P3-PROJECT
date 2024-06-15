430fd3848b570723f20aefa3cdf5f905fceed19e
tonymk0987 <k.tony.muriuki@gmail.com>
2024-06-15 03:30:32 +0300

    Initial commit

--- seed.py
+++ seed.py
@@ -1,18 +1,35 @@
 # Import the classes
 from bible import Bible
 from books import Book
-
-# Create the tables
-Bible.create_table()
-Book.create_table()
-
-# Create a Bible instance
+# Create 6 Bible instances
 bible1 = Bible.create('Old Testament', 'Law')
+bible2 = Bible.create('Old Testament', 'Prophets')
+bible3 = Bible.create('Old Testament', 'Wisdom')
+bible4 = Bible.create('Old Testament', 'History')
+bible5 = Bible.create('New Testament', 'Gospels')
+bible6 = Bible.create('New Testament', 'Epistles')
 
-# Create some Book instances
+# Create Book instances associated with each Bible
 book1 = Book.create('Genesis', bible1.id)
 book2 = Book.create('Exodus', bible1.id)
 
-# Retrieve all books for a given Bible instance
+book3 = Book.create('Isaiah', bible2.id)
+book4 = Book.create('Jeremiah', bible2.id)
+
+book5 = Book.create('Psalms', bible3.id)
+
+book6 = Book.create('Joshua', bible4.id)
+book7 = Book.create('Judges', bible4.id)
+
+book8 = Book.create('Matthew', bible5.id)
+book9 = Book.create('Mark', bible5.id)
+book10 = Book.create('Luke', bible5.id)
+book11 = Book.create('John', bible5.id)
+
+book12 = Book.create('Romans', bible6.id)
+book13 = Book.create('1 Corinthians', bible6.id)
+book14 = Book.create('2 Corinthians', bible6.id)
+
+# Example: Retrieve all books for a given Bible instance
 books_in_bible1 = bible1.books()
 print(books_in_bible1)
