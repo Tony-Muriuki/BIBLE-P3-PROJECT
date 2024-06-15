e0e57bd87b0fd2030eca4b6f797eefdf7d386ca2
tonymk0987 <k.tony.muriuki@gmail.com>
2024-06-15 06:46:05 +0300

    Updated cli file

--- cli.py
+++ cli.py
@@ -1,2 +1,71 @@
+from function import (
+    exit_program,
+    list_bibles,
+    find_bible_by_name,
+    find_bible_by_id,
+    create_bible,
+    update_bible,
+    delete_bible,
+    list_books,
+    find_book_by_name,
+    find_book_by_id,
+    create_book,
+    update_book,
+    delete_book,
+    list_bible_books
+)
+
+def main():
+    while True:
+        menu()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            list_bibles()
+        elif choice == "2":
+            find_bible_by_name()
+        elif choice == "3":
+            find_bible_by_id()
+        elif choice == "4":
+            create_bible()
+        elif choice == "5":
+            update_bible()
+        elif choice == "6":
+            delete_bible()
+        elif choice == "7":
+            list_books()
+        elif choice == "8":
+            find_book_by_name()
+        elif choice == "9":
+            find_book_by_id()
+        elif choice == "10":
+            create_book()
+        elif choice == "11":
+            update_book()
+        elif choice == "12":
+            delete_book()
+        elif choice == "13":
+            list_bible_books()
+        else:
+            print("Invalid choice")
+
+def menu():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. List all Bibles")
+    print("2. Find Bible by name")
+    print("3. Find Bible by id")
+    print("4. Create Bible")
+    print("5. Update Bible")
+    print("6. Delete Bible")
+    print("7. List all Books")
+    print("8. Find Book by name")
+    print("9. Find Book by id")
+    print("10. Create Book")
+    print("11. Update Book")
+    print("12. Delete Book")
+    print("13. List all Books in a Bible")
+
 if __name__ == "__main__":
-    pass
+    main()
---
+++ function.py
@@ -0,0 +1,111 @@
+# from bible import Bible
+# from books import Book
+
+# def exit_program():
+#     print("SHALOM!, MAY THE PEACE OF THE LORD BE UPON YOU BRETHREN")
+#     exit()
+
+# def list_bibles():
+#     bibles = Bible.get_all()
+#     for bible in bibles:
+#         print(bible)
+
+# def find_bible_by_name():
+#     name = input("Enter the Bible's name: ")
+#     bible = Bible.find_by_name(name)
+#     print(bible) if bible else print(f'Bible {name} not found')
+
+# def find_bible_by_id():
+#     id_ = input("Enter the Bible's id: ")
+#     bible = Bible.find_by_id(id_)
+#     print(bible) if bible else print(f'Bible {id_} not found')
+
+# def create_bible():
+#     testament = input("Enter the Bible's testament: ")
+#     category = input("Enter the Bible's category: ")
+#     try:
+#         bible = Bible.create(testament, category)
+#         print(f'Success: {bible}')
+#     except Exception as exc:
+#         print("Error creating Bible: ", exc)
+
+# def update_bible():
+#     id_ = input("Enter the Bible's id: ")
+#     if bible := Bible.find_by_id(id_):
+#         try:
+#             testament = input("Enter the Bible's new testament: ")
+#             bible.testament = testament
+#             category = input("Enter the Bible's new category: ")
+#             bible.category = category
+
+#             bible.update()
+#             print(f'Success: {bible}')
+#         except Exception as exc:
+#             print("Error updating Bible: ", exc)
+#     else:
+#         print(f'Bible {id_} not found')
+
+# def delete_bible():
+#     id_ = input("Enter the Bible's id: ")
+#     if bible := Bible.find_by_id(id_):
+#         bible.delete()
+#         print(f'Bible {id_} deleted')
+#     else:
+#         print(f'Bible {id_} not found')
+
+# def list_books():
+#     books = Book.get_all()
+#     for book in books:
+#         print(book)
+
+# def find_book_by_name():
+#     name = input("Enter the Book's name: ")
+#     book = Book.find_by_name(name)
+#     print(book) if book else print(f'Book {name} not found')
+
+# def find_book_by_id():
+#     id_ = input("Enter the Book's id: ")
+#     book = Book.find_by_id(id_)
+#     print(book) if book else print(f'Book {id_} not found')
+
+# def create_book():
+#     name = input("Enter the Book's name: ")
+#     bible_id = input("Enter the Book's Bible id: ")
+#     try:
+#         book = Book.create(name, bible_id)
+#         print(f'Success: {book}')
+#     except Exception as exc:
+#         print("Error creating Book: ", exc)
+
+# def update_book():
+#     id_ = input("Enter the Book's id: ")
+#     if book := Book.find_by_id(id_):
+#         try:
+#             name = input("Enter the Book's new name: ")
+#             book.name = name
+#             bible_id = input("Enter the Book's new Bible id: ")
+#             book.bible_id = bible_id
+
+#             book.update()
+#             print(f'Success: {book}')
+#         except Exception as exc:
+#             print("Error updating Book: ", exc)
+#     else:
+#         print(f'Book {id_} not found')
+
+# def delete_book():
+#     id_ = input("Enter the Book's id: ")
+#     if book := Book.find_by_id(id_):
+#         book.delete()
+#         print(f'Book {id_} deleted')
+#     else:
+#         print(f'Book {id_} not found')
+
+# def list_bible_books():
+#     bible_id = input("Enter the Bible's id: ")
+#     books = Book.find_by_bible_id(bible_id)
+#     if books:
+#         for book in books:
+#             print(book)
+#     else:
+#         print(f'No books found for Bible {bible_id}')
